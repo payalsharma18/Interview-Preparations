141c44229b6debc3fa4e559083c6388ff9a6d529
Payal Sharma <131345254+payalsharma18@users.noreply.github.com>
2024-06-05 10:50:52 +0530

    Next.js

---
+++ Next.js
@@ -0,0 +1,6 @@
+//Next.js
+1. What is Next JS?
+Next JS is an open-source web development React-based framework created by Vercel, which is famous for its unique features such as Server-side rendering and enhanced SEO. It has some additional features such as data fetching utilities, dynamic API routes, optimized builds, etc. It is a framework built upon React, Webpack, and Babel.
+
+
+  
