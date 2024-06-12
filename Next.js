051c2d65cb9007519baa429454cb5e98bf04f989
Payal Sharma <131345254+payalsharma18@users.noreply.github.com>
2024-06-12 12:47:00 +0530

    12-06-2024

--- Next.js
+++ Next.js
@@ -1,7 +1,6 @@
 //Next.js - 5th June 2024
 What is Next JS?
 Worked on Lazy loading 
-Working on Multilingual Support in my demo project
 Diifernces between CSR and SSR.
 Difference between reactjs and nextjs
 Routing
